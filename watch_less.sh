7c7b3d39befb7499e2e018764734f06ba96491e7
Juan Olvera <olverajuanluis@gmail.com>
2013-04-21 20:20:24 -0500

    deleted unnecesary files

--- watch_less.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Requires watchr: https://github.com/mynyml/watchr
-watchr -e 'watch(".*\.less$") { |f| system("lessc #{f[0]} > $(echo #{f[0]} | cut -d\. -f1).css && echo \"#{f[0]} > $(echo #{f[0]} | cut -d\. -f1).css\" ") }'
